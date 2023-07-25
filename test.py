ea8e9263f078f5f1cab0fbbef3ef7c8715d79cf4
kiyoungkim2 <85026353+kiyoungkim2@users.noreply.github.com>
2023-07-25 11:54:53 +0900

    re structuring

---
+++ test.py
@@ -0,0 +1,99 @@
+#-*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+
+from transformers import BertTokenizerFast, BertModel
+from torch.utils.data import Dataset
+
+from sklearn.metrics import f1_score
+
+res = open('res.txt', 'w')
+
+#model 클래스 정의
+class BertBaseModel(nn.Module):
+    def __init__(self):
+        super(BertBaseModel, self).__init__()
+        self.bert = BertModel.from_pretrained("kykim/bert-kor-base")
+        self.cls = nn.Linear(771, 4)
+    def forward(self, data, plus):
+        out = self.cls(torch.cat((self.bert(data)[1], plus), -1))
+
+        return(out)
+
+
+#Dataset 클래스 정의
+class NewsDataset(Dataset):
+    def __init__(self, txt_file, cer_file, pol_file, tense_file):
+        self.text = [e.split('\t')[0] for e in open(txt_file, 'r', encoding='utf8').readlines()]
+        self.classtype = [e.split('\t')[1] for e in open(txt_file, 'r', encoding='utf8').readlines()]
+        self.certype = [e.split('\t')[1].strip('\n') for e in open(cer_file, 'r', encoding='utf8').readlines()]
+        self.poltype = [e.split('\t')[1].strip('\n') for e in open(pol_file, 'r', encoding='utf8').readlines()]
+        self.tensetype = [e.split('\t')[1].strip('\n') for e in open(tense_file, 'r', encoding='utf8').readlines()]
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, idx):
+        text = self.text[idx]
+        classtype = int(float(self.classtype[idx]))
+        certype = int(float(self.certype[idx]))
+        poltype = int(float(self.poltype[idx]))
+        tensetype = int(float(self.tensetype[idx]))
+
+        return text, classtype, certype, poltype, tensetype
+
+#학습 준비
+device = 'gpu' if torch.cuda.is_available() else 'cpu'
+
+tokenizer_bert = BertTokenizerFast.from_pretrained("kykim/bert-kor-base")
+
+model_cls = BertBaseModel()
+model_state_dict = torch.load("./model_state_dict.pt", map_location=device)
+model_cls.load_state_dict(model_state_dict)
+
+model_cls.to(device)
+model_cls.eval()
+
+#test 진행
+count = 0
+right_count = 0
+
+dataset_test = NewsDataset('data_nia/test.txt', 'data_nia/test_cer.txt', 'data_nia/test_pol.txt', 'data_nia/test_tense.txt')
+data_loader_test = DataLoader(dataset_test, 1, shuffle=True)
+torch.save(model_cls, './model')
+model_cls.eval()
+
+src_list = []
+tgt_list = []
+
+for i, d in enumerate(data_loader_test):
+    count += 1
+    text, classtype, certype, poltype, tensetype = d
+
+    text = list(text)
+    certype = certype.unsqueeze(1)
+    poltype = poltype.unsqueeze(1)
+    tensetype = tensetype.unsqueeze(1)
+
+    inp_plus = torch.cat((certype, poltype, tensetype), -1).to(device)
+    input = tokenizer_bert(text, return_tensors="pt", padding=True)
+    outputs_cls = model_cls(input["input_ids"].to(device), inp_plus)[0].cpu()
+
+    if classtype == torch.argmax(outputs_cls):
+        right_count += 1
+    #print(classtype, torch.argmax(outputs_cls))
+
+    p = classtype.tolist()[0]
+    q = torch.argmax(outputs_cls).tolist()
+
+    res.write('정답: ' + str(p) + ', 결과: ' + str(q) + '\n')
+
+    src_list.append(p)
+    tgt_list.append(q)
+
+print(right_count/count)
+f_score = f1_score(src_list, tgt_list, average='macro')
+print(f_score)
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
-import torch 
+#-*- coding: utf-8 -*-
+
+import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.optim import Adam
@@ -7,13 +9,10 @@ from transformers import BertTokenizerFast, BertModel
 from torch.utils.data import Dataset
 
 from sklearn.metrics import f1_score
-import time
 
-print('2023-03-22-22:53')
 res = open('res.txt', 'w')
 
 #model 클래스 정의
-#속성까지 input 하는 경우 classifier input size 를 771 로 수정 필요. 
 class BertBaseModel(nn.Module):
     def __init__(self):
         super(BertBaseModel, self).__init__()
@@ -24,14 +23,15 @@ class BertBaseModel(nn.Module):
 
         return(out)
 
+
 #Dataset 클래스 정의
 class NewsDataset(Dataset):
     def __init__(self, txt_file, cer_file, pol_file, tense_file):
-        self.text = [e.split('\t')[0] for e in open(txt_file, 'r').readlines()]
-        self.classtype = [e.split('\t')[1] for e in open(txt_file, 'r').readlines()]
-        self.certype = [e.split('\t')[1].strip('\n') for e in open(cer_file, 'r').readlines()]
-        self.poltype = [e.split('\t')[1].strip('\n') for e in open(pol_file, 'r').readlines()]
-        self.tensetype = [e.split('\t')[1].strip('\n') for e in open(tense_file, 'r').readlines()]
+        self.text = [e.split('\t')[0] for e in open(txt_file, 'r', encoding='utf8').readlines()]
+        self.classtype = [e.split('\t')[1] for e in open(txt_file, 'r', encoding='utf8').readlines()]
+        self.certype = [e.split('\t')[1].strip('\n') for e in open(cer_file, 'r', encoding='utf8').readlines()]
+        self.poltype = [e.split('\t')[1].strip('\n') for e in open(pol_file, 'r', encoding='utf8').readlines()]
+        self.tensetype = [e.split('\t')[1].strip('\n') for e in open(tense_file, 'r', encoding='utf8').readlines()]
 
     def __len__(self):
         return len(self.text)
@@ -46,10 +46,12 @@ class NewsDataset(Dataset):
         return text, classtype, certype, poltype, tensetype
 
 #학습 준비
+device = 'gpu' if torch.cuda.is_available() else 'cpu'
+
 tokenizer_bert = BertTokenizerFast.from_pretrained("kykim/bert-kor-base")
 
 model_cls = BertBaseModel()
-model_cls.cuda()
+model_cls.to(device)
 model_cls.train()
 
 dataset = NewsDataset('data_nia/train.txt', 'data_nia/train_cer.txt', 'data_nia/train_pol.txt', 'data_nia/train_tense.txt')
@@ -77,19 +79,15 @@ for e in range(epoch):
         poltype = poltype.unsqueeze(1)
         tensetype = tensetype.unsqueeze(1)
 
-        inp_plus = torch.cat((certype, poltype, tensetype), -1).cuda()
-        
+        inp_plus = torch.cat((certype, poltype, tensetype), -1).to(device)
 
         text = list(text)
         input = tokenizer_bert(text, return_tensors="pt", padding=True)
-        outputs_cls = model_cls(input["input_ids"].cuda(), inp_plus)
+        outputs_cls = model_cls(input["input_ids"].to(device), inp_plus)
 
-        loss_cls = criterion(outputs_cls, classtype.cuda())
-
+        loss_cls = criterion(outputs_cls, classtype.to(device))
 
         loss_cls.backward()
-
-
         optimizer_cls.step()
 
         running_loss_cls += loss_cls.item()
@@ -98,46 +96,6 @@ for e in range(epoch):
             print('___________________________')
             print("running_loss_cls: ", running_loss_cls)
 
-            running_loss_cls = 0.0 
+            running_loss_cls = 0.0
 
-
-#test 진행
-count = 0
-right_count = 0 
-
-dataset_test = NewsDataset('data_nia/test.txt', 'data_nia/test_cer.txt', 'data_nia/test_pol.txt', 'data_nia/test_tense.txt')
-data_loader_test = DataLoader(dataset_test, 1, shuffle=True)
-torch.save(model_cls, './model')
-model_cls.eval()
-
-src_list = []
-tgt_list = []
-
-for i, d in enumerate(data_loader_test):
-    count += 1
-    text, classtype, certype, poltype, tensetype = d
-
-    text = list(text)
-    certype = certype.unsqueeze(1)
-    poltype = poltype.unsqueeze(1)
-    tensetype = tensetype.unsqueeze(1)
-
-    inp_plus = torch.cat((certype, poltype, tensetype), -1).cuda()
-    input = tokenizer_bert(text, return_tensors="pt", padding=True)
-    outputs_cls = model_cls(input["input_ids"].cuda(), inp_plus)[0].cpu()
-
-    if classtype == torch.argmax(outputs_cls):
-        right_count += 1
-    #print(classtype, torch.argmax(outputs_cls))
-
-    p = classtype.tolist()[0]
-    q = torch.argmax(outputs_cls).tolist()
-
-    res.write('정답: ' + str(p) + ', 결과: ' + str(q) + '\n')
-
-    src_list.append(p)
-    tgt_list.append(q)
-
-print(right_count/count)
-f_score = f1_score(src_list, tgt_list, average='macro')
-print(f_score)
+torch.save(model_cls.state_dict(), "./model_state_dict.pt")
