2800b1a2885b49a76545a0fb6e550bca1568a4a0
kiyoungkim2 <85026353+kiyoungkim2@users.noreply.github.com>
2023-07-26 13:00:21 +0900

    update

--- train.py
+++ train.py
@@ -2,6 +2,6 @@ import trainer
 
 
 CLS = trainer.Classification(model_name='kykim/bert-kor-base', min_sentence_length=5, MAX_LEN=256, batch_size=32, use_bert_tokenizer=True)
-CLS.dataset(data_path='data_nia/data.tsv', col_sentence='text', col_label='label')
+CLS.dataset(data_path='data/data.tsv', col_sentence='text', col_label='label')
 CLS.load_model(mode='train')
 CLS.train(epochs=3, dataset_split=0.1)
