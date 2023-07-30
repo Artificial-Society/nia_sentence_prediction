3e15eac7a2acdb75bdf65ef54d586340323f3604
kiyoungkim2 <kykim@artificial.sc>
2023-07-30 22:30:45 +0900

    docker

---
+++ dockerfile
@@ -0,0 +1,15 @@
+# 기본 이미지로 PyTorch가 설치된 Python 이미지를 사용
+FROM pytorch/pytorch:1.9.0-cuda10.2-cudnn7-runtime
+
+# 작업 디렉토리 설정
+WORKDIR /app
+
+# 필요한 패키지를 설치
+RUN pip install torch transformers scikit-learn tensorboard ml-things pandas
+
+# 현재 디렉토리의 모든 파일을 /app에 복사
+COPY . /app
+
+# 기본 명령 실행
+CMD ["python", "main.py"]
+
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import classifier
 # {'사실형': 0, '추론형': 1, '대화형': 2, '예측형': 3}
 
 # train
-CLS = classifier.Classification(model_name='kykim/bert-kor-base', min_sentence_length=5, MAX_LEN=128, batch_size=32, use_bert_tokenizer=True)
+CLS = classifier.Classification(model_name='kykim/bert-kor-base', min_sentence_length=5, MAX_LEN=64, batch_size=32, use_bert_tokenizer=True)
 CLS.dataset(data_path='data/data_sample.tsv', col_sentence='text', col_label='label')
 CLS.load_model(mode='train')
-CLS.train(epochs=3, dataset_split=0.1)
+CLS.train(epochs=2, dataset_split=0.1)
 
 # # inference
 # sentences = ['내일은 비가 올 것으로 예상됩니다.']
